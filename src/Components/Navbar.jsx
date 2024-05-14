036f5e8925793cb5107feffdb60f5779246babbe
meetkhatu <143870423+meetkhatu@users.noreply.github.com>
2024-05-14 18:10:49 +0530

    updates_final

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,8 +12,8 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-          <Nav.Link href="/" className='ms-3 fs-4'>Home</Nav.Link>
-          <Nav.Link href="/books" className='ms-3 fs-4'>Books</Nav.Link>
+          <Link to="/" style={{textDecoration:"none"}}><li href="/" className='text-black ms-3 fs-4'>Home</li></Link>
+          <Link to="/books" style={{textDecoration:"none"}}><li href="/books" className='text-black ms-3 fs-4'>Books</li></Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
